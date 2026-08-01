16e75e4394e32029bea84831b6e1209bb19b50ff
Dibyajyoti Chakrabarti <dibyajyoti.chakrabarti.122@gmail.com>
2026-08-01 12:24:27 +0000

    fix: add /health route for ALB health check

--- backend/todoapp/urls.py
+++ backend/todoapp/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('health', lambda request: HttpResponse('ok')),
     path('api/todos/', include('todos.urls')),
     path('api/payments/', include('payments.urls')),
 ]
